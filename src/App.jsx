5e295bd7b032d833b4ecf47712786616c26572f5
Akarsh Nair <akarshnair007@gmail.com>
2023-10-18 09:07:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Hero from "./Components/Hero";
+import "./Components/Navigation"
+import Navigation from "./Components/Navigation";
+const App = () =>{
+return(
+<div>
+
+<Navigation />
+<Hero />
+
+</div>
+);
+};
+export default App;
